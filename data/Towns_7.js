de68858bfa3c6b01e29469b2ab66884c0a179e8f
LeoStalin <39996089+LeoStalin@users.noreply.github.com>
2020-08-28 16:25:55 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,14 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Neyveli" }, "geometry": { "type": "Point", "coordinates": [ 79.483843007144543, 11.53300000316133 ] } },
+{ "type": "Feature", "properties": { "NAME": "Chidambaram" }, "geometry": { "type": "Point", "coordinates": [ 79.694864171060004, 11.39757859426946 ] } },
+{ "type": "Feature", "properties": { "NAME": "Cuddalore" }, "geometry": { "type": "Point", "coordinates": [ 79.7498551182954, 11.749457084633089 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kurinjipadi" }, "geometry": { "type": "Point", "coordinates": [ 79.593503909978494, 11.56179613520284 ] } },
+{ "type": "Feature", "properties": { "NAME": "Virudhachalam" }, "geometry": { "type": "Point", "coordinates": [ 79.33113441495216, 11.52329458943192 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tittagudi" }, "geometry": { "type": "Point", "coordinates": [ 79.120090353937826, 11.405355608606291 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kattumannarkoil" }, "geometry": { "type": "Point", "coordinates": [ 79.548190348957675, 11.28266513698588 ] } }
+]
+}
